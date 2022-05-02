b0b3329d65583f884259f3d84fe1e4ee40e2ee33
Varshinee Sreekanth <varshinee@utexas.edu>
2022-05-02 16:11:09 -0500

    Updated download scripts to copy from drive

--- rubi/datasets/scripts/download_vqa2.sh
+++ rubi/datasets/scripts/download_vqa2.sh
@@ -1,11 +1,16 @@
 mkdir -p data/vqa
 cd data/vqa
-wget http://data.lip6.fr/cadene/block/vqa2.tar.gz
-wget http://data.lip6.fr/cadene/block/coco.tar.gz
+# wget http://data.lip6.fr/cadene/block/vqa2.tar.gz
+# wget http://data.lip6.fr/cadene/block/coco.tar.gz
+cp -r /content/drive/MyDrive/2021-2022/GNLP/GNLP-Final-Project/RUBi/coco.tar.gz ./
+cp -r /content/drive/MyDrive/2021-2022/GNLP/GNLP-Final-Project/RUBi/vqa2.tar.gz ./
 tar -xzvf vqa2.tar.gz
 tar -xzvf coco.tar.gz
 
+cd ../..
+
 mkdir -p data/vqa/coco/extract_rcnn
 cd data/vqa/coco/extract_rcnn
-wget http://data.lip6.fr/cadene/block/coco/extract_rcnn/2018-04-27_bottom-up-attention_fixed_36.tar
+cp -r /content/drive/MyDrive/2021-2022/GNLP/GNLP-Final-Project/RUBi/2018-04-27_bottom-up-attention_fixed_36.tar ./
+# wget http://data.lip6.fr/cadene/block/coco/extract_rcnn/2018-04-27_bottom-up-attention_fixed_36.tar
 tar -xvf 2018-04-27_bottom-up-attention_fixed_36.tar
--- rubi/datasets/scripts/download_vqacp2.sh
+++ rubi/datasets/scripts/download_vqacp2.sh
@@ -1,4 +1,5 @@
 mkdir -p data/vqa
 cd data/vqa
-wget http://data.lip6.fr/cadene/murel/vqacp2.tar.gz
+cp -r /content/drive/MyDrive/2021-2022/GNLP/GNLP-Final-Project/RUBi/vqacp2.tar.gz ./
+# wget http://data.lip6.fr/cadene/murel/vqacp2.tar.gz
 tar -xzvf vqacp2.tar.gz
