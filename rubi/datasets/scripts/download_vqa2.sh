ada3f9ee8998399f0451691304fddd8bb6ec5052
Varshinee Sreekanth <varshinee@utexas.edu>
2022-05-08 19:15:59 -0500

    Fixed directory traversing

--- rubi/datasets/scripts/download_vqa2.sh
+++ rubi/datasets/scripts/download_vqa2.sh
@@ -2,7 +2,7 @@ mkdir -p data/vqa
 cd data/vqa
 # wget http://data.lip6.fr/cadene/block/vqa2.tar.gz
 # wget http://data.lip6.fr/cadene/block/coco.tar.gz
-mv -r ../../coco.tar.gz ./
+mv -r ../../../coco.tar.gz ./
 mv -r ../../vqa2.tar.gz ./
 tar -xzvf vqa2.tar.gz
 tar -xzvf coco.tar.gz
@@ -11,6 +11,6 @@ cd ../..
 
 mkdir -p data/vqa/coco/extract_rcnn
 cd data/vqa/coco/extract_rcnn
-mv -r ../../../../2018-04-27_bottom-up-attention_fixed_36.tar ./
+mv -r ../../../../../2018-04-27_bottom-up-attention_fixed_36.tar ./
 # wget http://data.lip6.fr/cadene/block/coco/extract_rcnn/2018-04-27_bottom-up-attention_fixed_36.tar
 tar -xvf 2018-04-27_bottom-up-attention_fixed_36.tar
